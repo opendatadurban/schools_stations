b7e160f9dc3364cf7fe952f235a2edda4965fc7b
heikoheilgendorff <heikoheilgendorff heikoheilgendorff@users.noreply.github.com>
2019-02-07 11:29:12 +0000

    first commit

---
+++ run_weatherstation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo python -u /home/pi/Desktop/schools_stations/weather_station.py > /home/pi/Desktop/schools_stations/station.output &
+#python -u weather_station.py > station.output &
+#python -u wtf.py > station.output &
